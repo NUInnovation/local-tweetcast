a468d619f2f9ef26768200537f018de2ca83e36d
Athif Wulandana <athif@u.northwestern.edu>
2016-04-25 19:57:33 -0500

    Basic form in flask

--- application.py
+++ application.py
@@ -1,5 +1,26 @@
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
-
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 app = Flask(__name__)
-app.config.from_object('config')
+
+class ReusableForm(Form):
+    name = TextField('Location: ', validators=[validators.required()])
+
+@app.route('/main', methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)
+
+    print(form.errors)
+    if request.method == 'POST':
+        location=request.form['location']
+        print(location)
+        if form.validate():
+            # Save the comment here.
+            flash('Hello ' + location)
+        else:
+            flash('Error: All the form fields are required. ')
+
+    return render_template('hello.html', form=form)
+
+if __name__ == '__main__':
+    app.run()
