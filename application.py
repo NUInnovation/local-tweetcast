ea05a9a37fdd52b9c3c42bf236851723c2ec4d36
Athif Wulandana <athif@u.northwestern.edu>
2016-04-25 20:27:04 -0500

    Location as a parameter in flask

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '<the super secret key comes here>'
 
 class ReusableForm(Form):
     name = TextField('Location: ', validators=[validators.required()])
@@ -12,15 +13,19 @@ def hello():
 
     print(form.errors)
     if request.method == 'POST':
-        location=request.form['location']
-        print(location)
+        loc=request.form['name']
+        print(loc)
         if form.validate():
             # Save the comment here.
-            flash('Hello ' + location)
+            flash('Hello ' + loc)
         else:
             flash('Error: All the form fields are required. ')
-
+        return redirect(url_for('results', location=loc))
     return render_template('hello.html', form=form)
 
+@app.route('/results/<location>')
+def results(location):
+    return location
 if __name__ == '__main__':
+    app.debug = True
     app.run()
