e95ac90e97b28a4ab17cb4b6388cfd80686d24a5
Sam Cohen <samc1213@gmail.com>
2016-05-02 19:03:49 -0500

    added stoplist to tf idf and created a predict candidate function

--- searching.py
+++ searching.py
@@ -14,6 +14,8 @@ from random import shuffle
 # logging for gensim
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+stoplist = [line.rstrip('\n') for line in open('stoplist.txt')]
+
 def add_to_gensim_dictionary_and_corpus(dictionary, corpus, id_to_path_dict, path_to_tweets_csv):
     # corpus should be a list (of lists), and dictionary must be a gensim dictionary
     block_of_tweets = ''
@@ -24,7 +26,7 @@ def add_to_gensim_dictionary_and_corpus(dictionary, corpus, id_to_path_dict, pat
             tweetstring = tweetrow[0]
             block_of_tweets += tweetstring
     clean_block_of_tweets = utils.any2unicode(block_of_tweets.replace('\n', ' ').replace('\t', ' '), errors='ignore')
-    text = [word for word in clean_block_of_tweets.lower().split()]
+    text = [word for word in clean_block_of_tweets.lower().split() if word not in stoplist]
     id_to_path_dict[len(corpus)] = path_to_tweets_csv
     dictionary.add_documents([text])
     corpus.append(dictionary.doc2bow(text))
@@ -51,6 +53,7 @@ def test_tfidf(training_set_size_fraction, k_neighbors):
     dictionary = corpora.Dictionary()
 
     for training_filepath in training_set:
+        print 'adding', training_filepath, 'to corpus/dictionary...'
         dictionary, corpus, id_to_path_dict = add_to_gensim_dictionary_and_corpus(dictionary, corpus, id_to_path_dict, training_filepath)
 
     tfidf = models.TfidfModel(corpus)
@@ -69,7 +72,7 @@ def test_tfidf(training_set_size_fraction, k_neighbors):
                 tweetstring = tweetrow[0]
                 block_of_tweets += tweetstring
         clean_block_of_tweets = utils.any2unicode(block_of_tweets.replace('\n', ' ').replace('\t', ' '), errors='ignore')
-        text = [word for word in clean_block_of_tweets.lower().split()]
+        text = [word for word in clean_block_of_tweets.lower().split() if word not in stoplist]
 
         doc_to_check = text
         vec_bow = dictionary.doc2bow(doc_to_check)
@@ -81,9 +84,36 @@ def test_tfidf(training_set_size_fraction, k_neighbors):
         else:
             wrongcount += 1
     return rightcount / float(rightcount + wrongcount)
+
+def predict_candidate(blob_of_tweets, k_neighbors):
+    corpus = []
+    id_to_path_dict = {}
+    dictionary = corpora.Dictionary()
+
+    for candidate_handle in candidate_handles:
+        candidate_folder = candidate_supporter_tweets_folders[candidate_handle]
+        dirlist = os.listdir(candidate_folder)
+        dirlist = [file for file in dirlist if file.endswith('.csv')]
+        shuffle(dirlist)
+        for i in range(len(dirlist)):
+            filepath = os.path.join(candidate_folder, dirlist[i])
+            print 'adding', training_filepath, 'to corpus/dictionary...'
+            dictionary, corpus, id_to_path_dict = add_to_gensim_dictionary_and_corpus(dictionary, corpus, id_to_path_dict, training_filepath)
+
+    clean_block_of_tweets = utils.any2unicode(blob_of_tweets.replace('\n', ' ').replace('\t', ' '), errors='ignore')
+    text = [word for word in clean_block_of_tweets.lower().split() if word not in stoplist]
+
+    doc_to_check = text
+    vec_bow = dictionary.doc2bow(doc_to_check)
+    vec_tfidf = tfidf[vec_bow] # convert the query to TDIDF space
+    sortedresult = sorted([(id_to_path_dict[tup[0]], tup[1]) for tup in list(enumerate(index[vec_tfidf]))], key=lambda x: x[1], reverse=True)
+    mode = max(set([tup[0].split('/')[0] for tup in sortedresult[:k_neighbors]]), key=[tup[0].split('/')[0] for tup in sortedresult[:k_neighbors]].count)
+    for candidate_handle, folder_name in candidate_supporter_tweets_folders.iteritems():
+        if folder_name == mode:
+            return candidate_handle
 testdict = {}
-for i in range(3, 30):
-    testdict[i] = test_tfidf(0.3, i)
+# for i in range(3, 30):
+testdict[7] = test_tfidf(0.3, 7)
 
 pprint(testdict)
 
